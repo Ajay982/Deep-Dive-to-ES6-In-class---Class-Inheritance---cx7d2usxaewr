67318c1b4cedb68371889b08ff70d863ecddd78f
Ajay982 <94911151+Ajay982@users.noreply.github.com>
2022-01-04 23:12:38 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+module.exports= class Shape{
+    constructor(color){
+        this.color =color;
+    }
+    drawShape(){
+        
+    }
+     calculateArea(){
+       
+    }
+}
+
