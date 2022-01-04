9d475feb62a82f61fdc9b692ec3c4a4a4224656e
Ajay982 <94911151+Ajay982@users.noreply.github.com>
2022-01-04 23:11:02 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,14 @@
 
 
 // export class using module.exports
+const {Shape} = require('.shape.js');
+ module.export=class Circle extends Shape{
+    constructor(radius,color="red"){
+        super(color)
+        this.radius =radius;
+    }
+    calculateArea(){
+        let pi=3.14;
+        return pi*radius*radius;
+    }
+}
