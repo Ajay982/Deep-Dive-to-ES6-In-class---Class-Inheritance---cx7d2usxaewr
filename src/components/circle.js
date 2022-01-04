96ace58f291fdfd1d877a1073278b4682181ccd4
Ajay982 <94911151+Ajay982@users.noreply.github.com>
2022-01-04 23:23:49 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,7 +4,7 @@
 
 
 // export class using module.exports
-const {Shape} = require('.shape.js');
+const Shape = require('.shape.js');
  module.export=class Circle extends Shape{
     constructor(radius,color="red"){
         super(color)
